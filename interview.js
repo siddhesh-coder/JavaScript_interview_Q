31c5f625105f9c9ccf504a212590b9a73611a585
Siddhesh Bhosale <72059095+siddhesh-coder@users.noreply.github.com>
2024-05-21 22:07:53 +0530

    Update interview.js

--- interview.js
+++ interview.js
@@ -390,3 +390,11 @@ y = y.split("").sort().join("");
 
 return x === y;
 }
+
+// Write a function that takes an array of objects and a key, and returns a new array sorted based on the values of that key in ascending order. 
+
+function sortByKey(arr, key) { 
+
+  return arr.sort((a, b) => a[key] – b[key]); 
+
+} 
